ffc4c552e08dc691ae05c78155f6ddee5c2a62e1
zcage <zcage@users.noreply.github.com>
2015-07-23 20:38:33 +0300

    Remove nCoinAge from GetProofOfStakeReward() in wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1982,7 +1982,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
         if (!txNew.GetCoinAge(txdb, nCoinAge))
             return error("CreateCoinStake : failed to calculate coin age");
 
-        int64_t nReward = GetProofOfStakeReward(nCoinAge, nFees);
+        int64_t nReward = GetProofOfStakeReward(nFees);
         if (nReward <= 0)
             return false;
 
