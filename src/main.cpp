00b9c1e36bc8ece960e4b3666cc2ec71aafac464
zcage <zcage@users.noreply.github.com>
2015-08-22 04:46:01 +0300

    ignore blocks received while importing

--- src/main.cpp
+++ src/main.cpp
@@ -3454,7 +3454,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
     }
 
 
-    else if (strCommand == "block")
+    else if (strCommand == "block" && !fImporting && !fReindex) // Ignore blocks received while importing
     {
         CBlock block;
         vRecv >> block;
